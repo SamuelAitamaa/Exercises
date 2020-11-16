0dd84ddd25d54c627eb1223a936b8d46197a342e
SamuelAitamaa <56270085+SamuelAitamaa@users.noreply.github.com>
2020-11-16 19:19:11 +0200

    async server updated

--- nodejs/asyncawait.js
+++ nodejs/asyncawait.js
@@ -61,10 +61,13 @@ app.get("/api/events", function (req, res) {
     })()
 });
 
+app.get('/events', function(req, res) {
+    res.sendFile(path.join(__dirname+'/listofevents.html'));
+})
+
 var server = app.listen(8080, function () {
     var host = server.address().address
     var port = server.address().port
-
-    console.log("Testi");
+    
     console.log("Example app listening at http://%s:%s", host, port)
 })
